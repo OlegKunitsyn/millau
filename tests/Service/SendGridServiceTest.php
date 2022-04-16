d0c690912727ad1f0ab69a9a42ee984f05b2d822
Olegs Kunicins <olegs.kunicins@gmail.com>
2022-04-16 13:59:45 +0200

    Add service tests

---
+++ tests/Service/SendGridServiceTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Service\SendGridService;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Mime\Address;
+
+class SendGridServiceTest extends WebTestCase
+{
+    private const TLD = 'millau.ovh';
+    private string $group;
+    private SendGridService $service;
+
+    public function setUp(): void
+    {
+        $client = static::createClient();
+        $client->disableReboot();
+        $this->service = $client->getContainer()->get(SendGridService::class);
+        $this->group = $client->getContainer()->get(ParameterBagInterface::class)->get('telegram_group');
+    }
+
+    public function testIsDomainValid()
+    {
+        $this->assertTrue($this->service->isDomainValid(self::TLD, $this->group));
+    }
+
+    public function testGetDomainRecords()
+    {
+        $records = $this->service->getDomainRecords(self::TLD, $this->group);
+        $this->assertCount(5, $records);
+    }
+
+    public function testSetInbound()
+    {
+        $this->assertTrue($this->service->setInbox(self::TLD, $this->group));
+    }
+
+    public function testGetRegisteredDomain()
+    {
+        $domain = $this->service->getRegisteredDomain(self::TLD, $this->group);
+        $this->assertEquals(self::TLD, $domain->domain);
+
+        $domain = $this->service->getRegisteredDomain('example.com', $this->group);
+        $this->assertNull($domain);
+    }
+
+    public function testParseInboundHtml()
+    {
+        $email = $this->service->parseInbound(json_decode(file_get_contents(__DIR__ . '/html.json'), true));
+        /** @var Address $from */
+        $from = $email->getFrom()[0];
+        $this->assertEquals('Firstname Lastname', $from->getName());
+        $this->assertEquals('firstname.lastname@gmail.com', $from->getAddress());
+        $this->assertEquals('Re: HTML email', $email->getSubject());
+        $this->assertTrue(strpos($email->getHtmlBody(), '<b>thank you for your Purchase Request.</b>') !== false);
+    }
+
+    public function testParseInboundText()
+    {
+        $email = $this->service::parseInbound(json_decode(file_get_contents(__DIR__ . '/text.json'), true));
+
+        /** @var Address[] $from */
+        $from = $email->getFrom();
+        $this->assertEquals('Firstname Lastname', $from[0]->getName());
+        $this->assertEquals('firstname.lastname@gmail.com', $from[0]->getAddress());
+
+        /** @var Address[] $to */
+        $to = $email->getTo();
+        $this->assertEquals('First Name', $to[0]->getName());
+        $this->assertEquals('first@example.com', $to[0]->getAddress());
+        $this->assertEquals('Last Name', $to[1]->getName());
+        $this->assertEquals('last@example.com', $to[1]->getAddress());
+
+        $this->assertEquals('Text email', $email->getSubject());
+        $this->assertEquals("Text body\n", $email->getTextBody());
+        $this->assertEquals("<div dir=\"ltr\">Text body<br></div>\n", $email->getHtmlBody());
+    }
+}
---
+++ tests/Service/TelegramServiceTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Service\SendGridService;
+use App\Service\TelegramService;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Mime\Address;
+
+class TelegramServiceTest extends WebTestCase
+{
+    private TelegramService $service;
+    private string $group;
+
+    public function setUp(): void
+    {
+        $client = static::createClient();
+        $client->disableReboot();
+        $this->service = $client->getContainer()->get(TelegramService::class);
+        $this->group = $client->getContainer()->get(ParameterBagInterface::class)->get('telegram_group');
+    }
+
+    public function testIsOnline()
+    {
+        $this->assertTrue($this->service->isOnline());
+    }
+
+    public function testCreatePostText()
+    {
+        $email = SendGridService::parseInbound(json_decode(file_get_contents(__DIR__ . '/text.json'), true));
+        $id = $this->service->createPostEmail($email, $this->group);
+        $this->assertNotNull($id);
+    }
+
+    public function testCreatePostHtml()
+    {
+        $email = SendGridService::parseInbound(json_decode(file_get_contents(__DIR__ . '/html.json'), true));
+        $id = $this->service->createPostEmail($email, $this->group);
+        $this->assertNotNull($id);
+    }
+
+    public function testCreatePostWelcome()
+    {
+        $email = SendGridService::parseInbound(json_decode(file_get_contents(__DIR__ . '/welcome.json'), true));
+        $id = $this->service->createPostEmail($email, $this->group);
+        $this->assertNotNull($id);
+    }
+
+    public function testCreatePostSib()
+    {
+        $email = SendGridService::parseInbound(json_decode(file_get_contents(__DIR__ . '/sib.json'), true));
+        $id = $this->service->createPostEmail($email, $this->group);
+        $this->assertNotNull($id);
+    }
+
+    public function testParseReply()
+    {
+        $email = $this->service->parseReply(json_decode(file_get_contents(__DIR__ . '/reply.json'), true));
+
+        /** @var Address[] $from */
+        $from = $email->getFrom();
+        $this->assertEquals('', $from[0]->getName());
+        $this->assertEquals('info@millau.ovh', $from[0]->getAddress());
+
+        /** @var Address[] $to */
+        $to = $email->getTo();
+        $this->assertEquals('Firstname Lastname', $to[0]->getName());
+        $this->assertEquals('firstname.lastname@gmail.com', $to[0]->getAddress());
+
+        $this->assertEquals('Re: Test subject', $email->getSubject());
+        $this->assertEquals("Hello!\nThis is my reply.\n:)", $email->getTextBody());
+        $this->assertEquals("Hello!<br />\nThis is my reply.<br />\n:)", $email->getHtmlBody());
+    }
+
+    public function testParseCommandE()
+    {
+        $email = $this->service->parseCommandE(json_decode(file_get_contents(__DIR__ . '/command.json'), true));
+
+        /** @var Address[] $from */
+        $from = $email->getFrom();
+        $this->assertEquals('', $from[0]->getName());
+        $this->assertEquals('firstname.lastname@gmail.com', $from[0]->getAddress());
+
+        /** @var Address[] $to */
+        $to = $email->getTo();
+        $this->assertEquals('', $to[0]->getName());
+        $this->assertEquals('info@millau.ovh', $to[0]->getAddress());
+
+        $this->assertEquals('Subject goes here', $email->getSubject());
+        $this->assertEquals("click reply", $email->getTextBody());
+        $this->assertEquals("click reply", $email->getHtmlBody());
+    }
+
+    public function testCreatePostEmail()
+    {
+        $email = $this->service->parseCommandE(json_decode(file_get_contents(__DIR__ . '/command.json'), true));
+        $id = $this->service->createPostEmail($email, $this->group);
+        $this->assertNotNull($id);
+    }
+}
